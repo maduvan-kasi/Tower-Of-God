e4660dfd9184da40065c5afed6729e837447629f
Kraton <kraton@MacBook-Pro.local>
2018-04-11 17:03:50 -0400

    Limited Card Draw, Updated Text#
    Limited Card Draw; Updated Text Displays
    
    Limited Card Draw; Updated Text Displays

--- Main.gd
+++ Main.gd
@@ -1,47 +1,34 @@
 extends Node
 
-var spot = 100
 var field = []
 var image = File.new()
 var loadedImage
 
+var names = ["Prince Keleseth", "Divine Commander", "Razorfen Hunter", "Argent Commander", "Prince Liam", "Stonetusk Golem", "Jaxxarus", "Tar Lord"]
+var pics = ["pk.jpg", "dc.png", "rh.jpg", "ac.png", "pl.png", "sc.jpg", "j.jpg", "tl.jpg"]
+
 export (PackedScene) var Card
+
+func draw_card():
+	var draw = Card.instance()
+	draw.NAME = str(randi() % len(names))
+	loadedImage = load("res://Card Art/" + pics[int(draw.NAME)])
+	draw.get_child(0).set_texture(loadedImage)
+	var sizeDraw = Vector2(loadedImage.get_size())
+	sizeDraw.x = 128 / sizeDraw.x
+	sizeDraw.y = 128 / sizeDraw.y
+	draw.get_child(0).set_scale(sizeDraw)
+	field.append(draw)
+	add_child(draw)
+	draw.connect("click", self, "draw_card")
+	draw.global_position = Vector2(randi() % 1024, randi() % 600)
+	draw.get_child(2).append_bbcode("[center][color=black]" + names[int(draw.NAME)] + "[/color][/center]")
+	names.remove(int(draw.NAME))
+	pics.remove(int(draw.NAME))
 
 func _ready():
 	randomize()
-	var draw = Card.instance()
-	draw.NAME = str(randi() % 8)
-	if image.file_exists("res://Card Art/" + draw.NAME + ".jpg"):
-		loadedImage = load("res://Card Art/" + draw.NAME + ".jpg")
-	else:
-		loadedImage = load("res://Card Art/" + draw.NAME + ".png")
-	draw.get_child(0).set_texture(loadedImage)
-	var sizeDraw = Vector2(loadedImage.get_size())
-	sizeDraw.x = 128 / sizeDraw.x
-	sizeDraw.y = 128 / sizeDraw.y
-	draw.get_child(0).set_scale(sizeDraw)
-	field.append(draw)
-	add_child(draw)
-	draw.connect("click", self, "_yeet")
-	draw.global_position = Vector2(randi() % 1024, randi() % 600)
+	draw_card()
 
 func _process(delta):
 	pass
-
-
-func _yeet():
-	var draw = Card.instance()
-	draw.NAME = str(randi() % 8)
-	if image.file_exists("res://Card Art/" + draw.NAME + ".jpg"):
-		loadedImage = load("res://Card Art/" + draw.NAME + ".jpg")
-	else:
-		loadedImage = load("res://Card Art/" + draw.NAME + ".png")
-	draw.get_child(0).set_texture(loadedImage)
-	var sizeDraw = Vector2(loadedImage.get_size())
-	sizeDraw.x = 128 / sizeDraw.x
-	sizeDraw.y = 128 / sizeDraw.y
-	draw.get_child(0).set_scale(sizeDraw)
-	field.append(draw)
-	add_child(draw)
-	draw.connect("click", self, "_yeet")
-	draw.global_position = Vector2(randi() % 1024, randi() % 600)
--- Test.gd
+++ Test.gd
@@ -13,17 +13,4 @@ func _input_event(viewport, event, shape_idx):
 		emit_signal("click")
 
 func _process(delta):
-	var velocity = Vector2() # the player's movement vector
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized()
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
+	pass
